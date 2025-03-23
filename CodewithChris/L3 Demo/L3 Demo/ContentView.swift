7e553f61e772fc933803a2df3a524ff7f7247d03
Rajesh Narayana Rao <mailrbn@gmail.com>
2025-03-23 12:18:22 +1100

    lesson 3 additional

--- CodewithChris/L3 Demo/L3 Demo/ContentView.swift
+++ CodewithChris/L3 Demo/L3 Demo/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0){
+                        Image("niagarafalls")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .cornerRadius(15)
+                        
+                        HStack() {
+                            Spacer()
+                            Text("Niagara Falls")
+                                .font(.title)
+                                .fontWeight(.bold)
+                            
+                            Spacer()
+                            VStack {
+                                HStack {
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.fill")
+                                    Image(systemName: "star.leadinghalf.filled")
+                                }
+                                Text("Reviews 361")
+                            }.foregroundColor(.orange)
+                                .font(.caption)
+                            Spacer()
+                        }
+                        
+                        Text("Come visit the falls for an experience of a lifetime")
+                        HStack {
+                            Spacer()
+                            Image(systemName: "fork.knife")
+                            Image(systemName: "binoculars.fill")
+                        }.foregroundColor(.gray)
+                            .font(.caption)
+                        
+                    }
+                    .padding()
+                    .background(Rectangle()
+                        .foregroundColor(.white)
+                        .cornerRadius(15)
+                        .shadow(radius: 15))
+                    .padding()
         }
-        .padding()
+        
+        
     }
 }
-
 #Preview {
     ContentView()
 }
